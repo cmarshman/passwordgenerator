cccc426c40cdac198fbd4b4e5eb382fee27ec26d
Cory Marshman <corymarshman@Corys-MacBook-Air.local>
2019-11-02 13:53:17 -0400

    variables

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// generate random password
-function generate (){
+var userChoice = ""
+var number = "1234567890"
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+
+var howMany = parseInt(prompt("How many characters would you like in your password"))
+
     
-    // set password length/complexity
-    let complexity = document.getElementById("slider")
-}
