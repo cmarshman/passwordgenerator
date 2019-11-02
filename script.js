093c1becaa6055c53ccfc0ad6bc09f9710516769
Cory Marshman <corymarshman@Corys-MacBook-Air.local>
2019-11-02 15:48:52 -0400

    almost there

--- script.js
+++ script.js
@@ -9,27 +9,50 @@ var howMany = parseInt(prompt("How many characters would you like in your passwo
 // if user selects 1-7 the receive noting back, if user selects 8-128 the user gets the ammount of characters they have selected
 if (howMany >= 8 && howMany <=128) {
     
-    var number = confirm("Would you like to include numbers in your password");
-    if (number === true){
-        userChoice += number 
+    var num = confirm("Would you like to include numbers in your password");
+    if (num === true){
+        userChoice += number;
     }
     console.log(userChoice)
 
-    var special = confirm("Would you like to include special characters in your password");
-    if (special === true){
-        userChoice += special
+    var spec = confirm("Would you like to include special characters in your password");
+    if (spec === true){
+        userChoice += special;
     }
     console.log(userChoice)
 
-    var upperCase = confirm("Would you like to include upper case letters in your password");
-    if (upperCase === true){
-        userChoice += upperCase
+    var upper = confirm("Would you like to include upper case letters in your password");
+    if (upper === true){
+        userChoice += upperCase;
     }
     console.log(userChoice)
 
-    var lowerCase = confirm("Would you like to include lower case letters in your password");
-    if (lowerCase === true){
-        userChoices += lowercase
+    var lower = confirm("Would you like to include lower case letters in your password");
+    if (lower === true){
+        userChoice += lowerCase;
     }
     console.log(userChoice)
 }
+
+else {
+    confirm("Please enter numbers between 8 and 128.")
+    document.location.reload()
+}
+
+function generate() {
+    var answer = "";
+    for (i = 1; i < howMany; i++) {
+        answer = answer + userChoice.charAt(Math.floor(Math.random() * Math.floor(userChoice.length - 1)))
+    }
+    console.log(answer)
+    document.getElementById("yourpassword").value = answer;
+    
+}
+
+function copyPassword() {
+    document.getElementById("yourpassword").select();
+    document.execCommand("Copy");
+    alert("Password is coppied to clipboard");
+}
+
+
